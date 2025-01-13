d17281fcd45feb3066643f875156e831ee31c5b4
juliapetroski <juliapetroski@usp.br>
2025-01-13 14:29:25 -0300

    Adding implementation doc files

---
+++ Implementation doc/@_Region file.R
@@ -0,0 +1,57 @@
+
+## Set repeated commands specific to the project region
+implementation <- "St_Peter_St_Paul"
+
+library(sf)
+library(ggplot2)
+
+#EPSG <- rgdal::make_EPSG()
+#EPSG2 <- dplyr::filter(EPSG, stringr::str_detect(note, "Brazil"))
+crs <- 5530                                                              # Specify the map projection for the project                                                     
+
+lims <- c(xmin = -44.25, xmax = -10, ymin = -14, ymax = 14)# Specify limits of plotting window, also used to clip data grids
+
+zoom <- coord_sf(xlim = c(lims[["xmin"]], lims[["xmax"]]), ylim = c(lims[["ymin"]], lims[["ymax"]]), expand = FALSE) # Specify the plotting window for SF maps in this region
+
+ggsave_map <- function(filename, plot) {
+  ggsave(filename, plot, scale = 1, width = 12, height = 10, units = "cm", dpi = 500, bg = "white")
+  
+}                             # Set a new default for saving maps in the correct size
+pre <- list(scale = 1, width = 12, height = 10, units = "cm", dpi = 500) # The same settings if you need to pass them to a function in MiMeMo.tools
+
+SDepth <- 60
+DDepth <- 600
+
+#### bathymetry.5 MODEL DOMAIN ####
+
+shape <- function(matrix) {
+  
+  shape <-  matrix %>% 
+    list() %>% 
+    st_polygon() %>% 
+    st_sfc() %>% 
+    st_sf(Region = implementation,.)
+  st_crs(shape) <- st_crs(4326)                                        
+  shape <- st_transform(shape, crs = crs)
+  return(shape)
+  
+}                      # Convert a matrix of lat-lons to an sf polygon
+
+#### expand polygon for sampling rivers ####
+
+river_expansion <- matrix(c(13, 73,
+                            0, 80,
+                            0, 85,
+                            63, 85,
+                            73, 77,
+                            30, 71,
+                            13, 73),
+                          ncol = 2, byrow = T) %>% 
+  list() %>% 
+  st_polygon() %>% 
+  st_sfc() %>% 
+  st_sf(Region = implementation,.)
+st_crs(river_expansion) <- st_crs(4326)                                          
+river_expansion <- st_transform(river_expansion, crs = crs)
+
+
